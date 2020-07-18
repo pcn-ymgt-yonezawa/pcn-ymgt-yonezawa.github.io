50ec3087fe04242bbf2a6e1549879512dd266bad
tomoya-shikanai <tomoya.shikanai@gmail.com>
2020-07-19 00:35:22 +0900

    add GA

---
+++ js/gtag.js
@@ -0,0 +1,6 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172981340-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());gtag('config', 'UA-172981340-1');
